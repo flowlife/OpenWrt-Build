ea03f5201f2a749184265d70f77f0f706af7993f
flowlife <flowlife@foxmail.com>
2022-12-13 14:09:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,5 @@ sed -i "/helloworld/d" "feeds.conf.default"
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 cat >>feeds.conf.default <<EOF
-src-git kenzo https://github.com/kenzok8/openwrt-packages.git
 src-git small https://github.com/kenzok8/small-package.git
 EOF
